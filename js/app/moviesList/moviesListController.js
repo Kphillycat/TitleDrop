5b1a6f3897f280332c95e717a39dc344f7be641f
Keith Williams Jr <keithwilliamsjr@gmail.com>
2015-07-01 13:43:12 -0400

    Refactor controller

--- js/app/moviesList/moviesListController.js
+++ js/app/moviesList/moviesListController.js
@@ -1,14 +1,17 @@
-angular.module('TitleDrop')
-	.controller('listController', ['grabMoviesService', function(grabMoviesService){
-		var vm = this;
-		// create movie array from service
-		grabMoviesService.success(function(data){
-			vm.movies = data;
-		}).
-		error(function(data, status, headers, config){
-			console.log('ERROR: ' + data);
-		});
-		// add movie time to database
-		// vm.addMovie = function(movie, time) {
-		// };
-	}]);
+angular.module('TitleDrop').controller('listController', listController);
+
+listController.$inject = ['grabMoviesService'];
+
+function listController(grabMoviesService){
+	var vm = this;
+	// create movie array from service
+	grabMoviesService.success(function(data){
+		vm.movies = data;
+	}).
+	error(function(data, status, headers, config){
+		console.log('ERROR: ' + data);
+	});
+	// add movie time to database
+	// vm.addMovie = function(movie, time) {
+	// };
+}
